7caa67084351df323f18717769b826d6547106cd
MahmoudAasi <mahmoud6aasi@gmail.com>
2025-07-15 23:19:27 +0300

    update package.json 1.0.11

--- src/web-component.tsx
+++ src/web-component.tsx
@@ -2,8 +2,6 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import reactToWebComponent from "react-to-webcomponent";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import AdStream from "./AdStream";
 import AdStreamCarousel from "./AdStreamCarousel";
 
